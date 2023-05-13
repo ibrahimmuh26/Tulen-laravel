60eec8d90e775dd80952cb9d1eaefe08311a2dd9
boim <ibrahimmuh26@gmail.com>
2023-05-13 17:54:14 +0700

    branch baru

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!-- Created By CodingLab - www.codinglabweb.com -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8" />
+    <title>CrowdMarket</title>
+    <link rel="stylesheet" href="login1.css" />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+    />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  </head>
+  <body>
+    <div class="container">
+      <form action="#">
+        <img
+          class="image-logo"
+          src="./public/logo-crowd-market2-1@2x.png"
+          alt=""
+        />
+
+        <div class="title">Selamat Datang!</div>
+        <div class="input-box underline">
+          <input type="text" placeholder="Enter Your Email" required />
+          <div class="underline"></div>
+        </div>
+        <div class="input-box">
+          <input type="password" placeholder="Enter Your Password" required />
+          <div class="underline"></div>
+        </div>
+        <div class="input-box button">
+          <input type="submit" name="" value="Continue" />
+        </div>
+      </form>
+      <div class="option">Belum punya akun? <a class="daftar" href="">daftar</a></div>
+    </div>
+  </body>
+</html>
